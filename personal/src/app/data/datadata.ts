711df1c23423c4aafd81ee0d93d2389094b6b0ca
Sultan-Al-Zoghaibi <ssultan007@icloud.com>
2025-01-05 17:46:00 -0700

    v1.07 bento pt2 color + data/datadata file

---
+++ personal/src/app/data/datadata.ts
@@ -0,0 +1,16 @@
+
+
+export const gridItems = [
+  {
+    title: "Title1", description: "Description1", id:1
+  },
+  {
+    title: "Title2", description: "Description2", id:2
+  },
+  {
+    title: "Title3", description: "Description3", id:3
+  },
+  {
+    title: "Title4", description: "Description4", id:4
+  },
+]
