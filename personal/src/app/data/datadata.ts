680f4ed092ce2b8b9082be7607430eb2fa7a24c7
Sultan-Al-Zoghaibi <ssultan007@icloud.com>
2025-01-07 16:00:47 -0700

    v1.10 bento id 2 all sized done,  added xs = mobile

--- personal/src/app/data/datadata.ts
+++ personal/src/app/data/datadata.ts
@@ -3,17 +3,17 @@ export const gridItems = [
     id: 1,
     title: "I prioritize client collaboration, fostering open communication ",
     description: "",
-    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    className: "lg:col-span-2 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/b1.svg",
+    img: "/",
     spareImg: "",
   },
   {
     id: 2,
     title: "My tech stack",
     description: "I constantly try to improve",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-4 lg:min-h-[60vh]", // Stays the same
+    className: "lg:col-span-3 md:col-span-4 md:row-span-4 lg:min-h-[60vh] xs:min-h-[50vh]",
     imgClassName: "",
     titleClassName: "justify-center",
     img: "",
@@ -23,27 +23,27 @@ export const gridItems = [
     id: 3,
     title: "Tech enthusiast with a passion for development.",
     description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-2 lg:min-h-[150px] md:min-h-[180px]", // Increased min height
+    className: "lg:col-span-2 md:col-span-2 md:row-span-2 lg:min-h-[150px] md:min-h-[180px]" , // Increased min height
     imgClassName: "",
     titleClassName: "justify-start",
-    img: "/grid.svg",
+    img: "",
     spareImg: "/b4.svg",
   },
   {
     id: 4,
-    title: "Currently building a JS Animation library",
+    title: "Currently helping TechStart migrate to Next.js",
     description: "The Inside Scoop",
-    className: "md:col-span-3 md:row-span-3 lg:min-h-[300px] md:min-h-[250px]", // Adjusted height for id: 4
+    className: "md:col-span-2 md:row-span-3 lg:min-h-[300px] md:min-h-[250px]", // Adjusted height for id: 4
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
+    img: "/",
     spareImg: "/grid.svg",
   },
   {
     id: 5,
     title: "Do you want to start a project together?",
     description: "",
-    className: "lg:col-span-2 md:col-span-3 md:row-span-1 lg:min-h-[150px] md:min-h-[130px]", // Explicit height for id: 5
+    className: "lg:col-span-3 md:col-span-4 md:row-span-1 lg:min-h-[150px] md:min-h-[130px]", // Explicit height for id: 5
     imgClassName: "",
     titleClassName: "justify-center md:max-w-full max-w-60 text-center",
     img: "",
