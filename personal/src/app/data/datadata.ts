961a4433f6a3419159155c41f66fb615b4b6e353
Sultan-Al-Zoghaibi <ssultan007@icloud.com>
2025-01-07 19:28:07 -0700

    v1.13 bento xs/sm/md/lg fine with these (fullcircle lol)

--- personal/src/app/data/datadata.ts
+++ personal/src/app/data/datadata.ts
@@ -1,29 +1,30 @@
 export const gridItems = [
+
   {
     id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
-    description: "",
-    className: "lg:col-span-2 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-    imgClassName: "w-full h-full",
-    titleClassName: "justify-end",
-    img: "/",
-    spareImg: "",
-  },
-  {
-    id: 2,
     title: "My tech stack",
     description: "I constantly try to improve",
-    className: "lg:col-span-3 md:col-span-4 md:row-span-4 lg:min-h-[60vh] xs:min-h-[50vh]",
+    className: "lg:col-span-3 md:col-span-4 md:row-span-4  lg:min-h-[60vh] xs:min-h-[60vh]",
     imgClassName: "",
     titleClassName: "justify-center",
     img: "",
     spareImg: "",
   },
   {
+    id: 2,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-2 md:col-span-2 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/",
+    spareImg: "",
+  },
+  {
     id: 3,
     title: "Tech enthusiast with a passion for development.",
     description: "",
-    className: "lg:col-span-2 md:col-span-2 md:row-span-2 lg:min-h-[150px] md:min-h-[180px]" , // Increased min height
+    className: "lg:col-span-2 lg:row-span-1 md:col-span-2 md:row-span-2 lg:max-h-[150px] lg:min-h-[150px] md:min-h-[180px] " , // Increased min height
     imgClassName: "",
     titleClassName: "justify-start",
     img: "",
@@ -33,7 +34,7 @@ export const gridItems = [
     id: 4,
     title: "Currently helping TechStart migrate to Next.js",
     description: "The Inside Scoop",
-    className: "md:col-span-2 md:row-span-3 lg:min-h-[300px] md:min-h-[250px]", // Adjusted height for id: 4
+    className: "lg:col-span-3 lg:row-span-2 md:col-span-4 md:row-span-3  lg:max-h-[340px] lg:min-h-[300px] md:min-h-[250px]", // Adjusted height for id: 4
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
     img: "/",
@@ -43,7 +44,7 @@ export const gridItems = [
     id: 5,
     title: "Do you want to start a project together?",
     description: "",
-    className: "lg:col-span-3 md:col-span-4 md:row-span-1 lg:min-h-[150px] md:min-h-[130px]", // Explicit height for id: 5
+    className: "lg:col-span-2 lg:row-span-1 md:col-span-1 md:row-span-1 lg:max-h-[150px] lg:min-h-[150px] md:min-h-[130px]", // Explicit height for id: 5
     imgClassName: "",
     titleClassName: "justify-center md:max-w-full max-w-60 text-center",
     img: "",
