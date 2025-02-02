628aba06658ebaab3fb1b515f80222dd4c04a6e7
30178597 <sultan.amrani@ucalgary.ca>
2025-02-02 13:57:15 -0700

    v1.50 added resume v1 and docker icon techstack

--- personal/src/app/component/Hero.tsx
+++ personal/src/app/component/Hero.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import React, { useEffect, useState } from "react";
 import BackgroundVid1 from "./backgroundvid1";
 import { Inter } from "next/font/google";
@@ -20,7 +20,6 @@ const interFont = Inter({
   subsets: ["latin"],
 });
 
-
 const Hero = () => {
   const [fadeIn, setFadeIn] = useState(false);
 
@@ -34,44 +33,56 @@ const Hero = () => {
 
   return (
     <div className="relative" id="home">
-  {/* Background video */}
-  <div className="absolute inset-0 -z-15">
-    <BackgroundVid1 />
-  </div>
+      {/* Background video */}
+      <div className="absolute inset-0 -z-15">
+        <BackgroundVid1 />
+      </div>
 
-  {/* Content */}
-  <div className="relative flex flex-col items-start lg:max-w-[800px] md:max-w-[800px] sm:max-w-[500px] max-w-[500px]  justify-start pt-20  pl-[50px]">
-    {/* Title text with generate effect */}
-    <TextGenerateEffect
-      className="font-stencilFont text-[70px] font-extrabold leading-none  lg: -right-[50px] "
-      words="Welcome to my site! Sultan Alzoghaibi"
-    />
+      {/* Content */}
+      <div className="relative flex flex-col items-start lg:max-w-[800px] md:max-w-[800px] sm:max-w-[500px] max-w-[500px]  justify-start pt-20  pl-[50px]">
+        {/* Title text with generate effect */}
+        <TextGenerateEffect
+          className="font-stencilFont text-[70px] font-extrabold leading-none  lg: -right-[50px] "
+          words="Welcome to my site! Sultan Alzoghaibi"
+        />
 
-    {/* Smaller text underneath with fade-in effect */}
-    <div
-      className={`max-w-[550px] transition-opacity duration-1000 ${
-        fadeIn ? "opacity-100" : "opacity-0"
-      }  mb-4 `}
-    >
-      <p className={`${interFont.className} text-lg text-white p-btom-4 mb-5`}>
-      I’m a student at the University of Calgary, enrolled in Finance, but I love studying software and programming. My goal is to graduate with a Computer Science degree. ^_^ !!!
-      </p>
-     
-      <span className="flex flex-wrap gap-x-4 gap-y-4 my-4">
-      <Magicbutton text="Email me" link="mailto:ssultan007@icloud.com" />
-      <LinkButton text="LinkedIn" icon="tech-logos/linkedin.png" link="https://www.linkedin.com/in/sultan-alzoghaibi-3b99a0250/" />
-      <LinkButton text="Github" icon="tech-logos/github-white-icon.webp" link="https://github.com/SultanAlzoghaibi" />
-      <LinkButton text="Output Online" icon="tech-logos/output-onlinepngtools.png" link="/THIS-IS-A-PLACEHOLDER.pdf" />
-      </span>
-     
-    
+        {/* Smaller text underneath with fade-in effect */}
+        <div
+          className={`max-w-[550px] transition-opacity duration-1000 ${
+            fadeIn ? "opacity-100" : "opacity-0"
+          }  mb-4 `}
+        >
+          <p
+            className={`${interFont.className} text-lg text-white p-btom-4 mb-5`}
+          >
+            I’m a student at the University of Calgary, enrolled in Finance, but
+            I love studying software and programming. My goal is to graduate
+            with a Computer Science degree. ^_^ !!!
+          </p>
+
+          <span className="flex flex-wrap gap-x-4 gap-y-4 my-4">
+            <Magicbutton text="Email me" link="mailto:ssultan007@icloud.com" />
+            <LinkButton
+              text="LinkedIn"
+              icon="tech-logos/linkedin.png"
+              link="https://www.linkedin.com/in/sultan-alzoghaibi-3b99a0250/"
+            />
+            <LinkButton
+              text="Github"
+              icon="tech-logos/github-white-icon.webp"
+              link="https://github.com/SultanAlzoghaibi"
+            />
+            <LinkButton
+              text="resume"
+              icon="tech-logos/output-onlinepngtools.png"
+              link="/Sultan-Alzoghaibi-Resume.pdf"
+            />
+          </span>
+        </div>
+
+        {/* Button */}
+      </div>
     </div>
-   
-
-    {/* Button */}
- 
-  </div>
-</div>
   );
 };
 
--- personal/src/app/component/ui/Bento-Grid.tsx
+++ personal/src/app/component/ui/Bento-Grid.tsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 import { useState } from "react";
 
 import { cn } from "@/lib/utils";
 import { div, object } from "framer-motion/client";
 import { BackgroundGradientAnimation } from "./bggradientanimation";
-import  Magicbutton  from "./Magicbutton";
-import {GridBackgroundDemo} from "./GridBackgroundDemo"
+import Magicbutton from "./Magicbutton";
+import { GridBackgroundDemo } from "./GridBackgroundDemo";
 import LinkButton from "../linkButton";
 
 const copied = false;
@@ -15,10 +15,8 @@ const defaultOptions = {
   autoplay: copied,
   rendererSettings: {
     preserveAspectRatio: "xMidYMid slice",
-    
   },
 };
-
 
 export const BentoGrid = ({
   className,
@@ -46,7 +44,7 @@ const leftLists = [
   { name: "React", icon: "/tech-logos/React.png" },
   { name: "TailwindCSS", icon: "/tech-logos/TailwindCSS.png" },
   { name: "Next.js", icon: "/tech-logos/nextjs.png" },
-  { name: "MongoDB", icon: "/tech-logos/MongoDB.png" },
+  { name: "Docker", icon: "/tech-logos/docker.webp" },
 ];
 
 const rightLists = [
@@ -56,7 +54,7 @@ const rightLists = [
   { name: "Python", icon: "/tech-logos/Python.png" },
   { name: "Java", icon: "/tech-logos/java.png" },
   { name: "Git", icon: "/tech-logos/git.png" },
-  { name: "API", icon: "/tech-logos/api.png" }, 
+  { name: "API", icon: "/tech-logos/api.png" },
 ];
 
 export const BentoGridItem = ({
@@ -79,7 +77,6 @@ export const BentoGridItem = ({
   spareImg?: string;
 }) => {
   return (
-    
     <div
       className={cn(
         "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
@@ -91,21 +88,23 @@ export const BentoGridItem = ({
         background: "linear-gradient(87deg, #1B1C1E 5.54%, #000 89.86%)",
       }}
     >
-      
-        
       <div className={`  h-full`}>
         <div className="w-full h-full absolute">
           {img && id !== 2 && (
             <img
               src={img}
               alt={img}
-              className={cn(imgClassName, "xs:opacity-30 lg:opacity-100 md:opacity-100 sm:opacity-100 object-cover object-center")}
+              className={cn(
+                imgClassName,
+                "xs:opacity-30 lg:opacity-100 md:opacity-100 sm:opacity-100 object-cover object-center"
+              )}
             />
           )}
         </div>
         <div
-          className={`absolute right-0 -bottom-5 ${id === 4 && "w-full opacity-80"
-            } `}
+          className={`absolute right-0 -bottom-5 ${
+            id === 4 && "w-full opacity-80"
+          } `}
         >
           {spareImg && (
             <img
@@ -115,135 +114,161 @@ export const BentoGridItem = ({
             />
           )}
         </div>
-        { id === 5 && (
-  <div className="relative z-1 group min-h-40 flex flex-col justify-center items-center">
-    {/* Background (no hover effect applied here) */}
-    <div className="absolute inset-0 w-full h-full z-0">
-      <BackgroundGradientAnimation />
-    </div>
+        {id === 5 && (
+          <div className="relative z-1 group min-h-40 flex flex-col justify-center items-center">
+            {/* Background (no hover effect applied here) */}
+            <div className="absolute inset-0 w-full h-full z-0">
+              <BackgroundGradientAnimation />
+            </div>
 
-    {/* Element that will be affected by hover */}
-    <div className="group-hover:translate-x-2 transition duration-200 relative flex flex-col justify-center items-center px-5 p-5 lg:p-2 z-10">
-      {/* Text */}
-      <div
-        className={cn(
-          titleClassName,
-          `text-white font-bold sm:px-0 md:px-2 px-2  mx-0
+            {/* Element that will be affected by hover */}
+            <div className="group-hover:translate-x-2 transition duration-200 relative flex flex-col justify-center items-center px-5 p-5 lg:p-2 z-10">
+              {/* Text */}
+              <div
+                className={cn(
+                  titleClassName,
+                  `text-white font-bold sm:px-0 md:px-2 px-2  mx-0
           text-2xl xs:text-lg sm:text-lg md:text-lg lg:text-2xl
           w-full text-center`
-        )}
-      >
-        {title}
-      </div>
+                )}
+              >
+                {title}
+              </div>
 
-      {/* Centered MagicButton */}
-      <div className="mt-2">
-        <LinkButton text="Github" icon="tech-logos/discord.webp" link="https://discord.com/users/h82luzn" />
-      </div>
-    </div>
-  </div>
-)}
-    
-
-    { id === 3 && (
-      <>
-       <div className="left-[-8] top-[-400] absolute w-full h-full ">
-    <GridBackgroundDemo />  
-    </div>
-      <div
-      className={cn(
-        titleClassName,
-        "group-hover/bento:translate-x-2 transition mb-0 duration-200 relative md:h-full min-h-40 flex flex-col px-5 pt-5 lg:pt-10"
-      )}
-    >
-      
-    <div >
-     
-    <div
-  className={`font-sans text-lg lg:text-2xl font-bold z-10 ${id === 3 ? 'sm:absolute xs:absolute xs:w-40 lg:absolute lg:w-60 lg:mb-5 lg:top-3 lg lg:text-2xl' : ''} ${id === 1 ? 'text-primary whitespace-nowrap' : 'text-white'}`}
->
-
-  {title}
-</div>
-    
-     <Magicbutton link="/myprojects/2" className="absolute bottom-0 mb-4 mr-9 " text="learn more" />
-    </div>
-    </div></>)
-    
-    
-    }
-
-    { id !== 5 && id !== 3 &&  <div
-          className={cn(
-            titleClassName,
-            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
-          )}
-        >
-
-          <div className="font-sans font-extralight sm:max-w-32 md:max-w-32 md:text-xs lg:text-base text-sm text-white z-10 flex flex-col gap-2">
-          <div className="font-sans font-extralight sm:max-w-32 md:max-w-32 md:text-xs lg:text-base text-sm text-white z-10">
-            {description}
+              {/* Centered MagicButton */}
+              <div className="mt-2">
+                <LinkButton
+                  text="Github"
+                  icon="tech-logos/discord.webp"
+                  link="https://discord.com/users/h82luzn"
+                />
+              </div>
+            </div>
           </div>
-          
+        )}
+
+        {id === 3 && (
+          <>
+            <div className="left-[-8] top-[-400] absolute w-full h-full ">
+              <GridBackgroundDemo />
+            </div>
+            <div
+              className={cn(
+                titleClassName,
+                "group-hover/bento:translate-x-2 transition mb-0 duration-200 relative md:h-full min-h-40 flex flex-col px-5 pt-5 lg:pt-10"
+              )}
+            >
+              <div>
+                <div
+                  className={`font-sans text-lg lg:text-2xl font-bold z-10 ${
+                    id === 3
+                      ? "sm:absolute xs:absolute xs:w-40 lg:absolute lg:w-60 lg:mb-5 lg:top-3 lg lg:text-2xl"
+                      : ""
+                  } ${
+                    id === 1 ? "text-primary whitespace-nowrap" : "text-white"
+                  }`}
+                >
+                  {title}
+                </div>
+
+                <Magicbutton
+                  link="/myprojects/2"
+                  className="absolute bottom-0 mb-4 mr-9 "
+                  text="learn more"
+                />
+              </div>
+            </div>
+          </>
+        )}
+
+        {id !== 5 && id !== 3 && (
           <div
-  className={`font-sans 
+            className={cn(
+              titleClassName,
+              "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+            )}
+          >
+            <div className="font-sans font-extralight sm:max-w-32 md:max-w-32 md:text-xs lg:text-base text-sm text-white z-10 flex flex-col gap-2">
+              <div className="font-sans font-extralight sm:max-w-32 md:max-w-32 md:text-xs lg:text-base text-sm text-white z-10">
+                {description}
+              </div>
+
+              <div
+                className={`font-sans 
     ${id === 2 ? " lg:w-40" : ""}
     text-lg lg:text-3xl max-w-96 font-bold z-10
-    ${id === 3 ? 'sm:absolute lg:absolute lg:w-60 lg:mb-5 lg:top-3 lg:text-2xl' : ''}
-    ${id === 1 ? 'text-primary whitespace-nowrap' : 'text-white'}
+    ${
+      id === 3
+        ? "sm:absolute lg:absolute lg:w-60 lg:mb-5 lg:top-3 lg:text-2xl"
+        : ""
+    }
+    ${id === 1 ? "text-primary whitespace-nowrap" : "text-white"}
   `}
->
-  {title}
-</div>
-</div>
+              >
+                {title}
+              </div>
+            </div>
 
-{id === 1 && (
-  <div className=" flex gap-1 lg:gap-2 w-fit absolute -right-0 xs:mt-100 sm:mt-60 md:mt-0 lg:mt-0"> 
-    <div className=" flex flex-col gap-2 md:gap-2 lg:gap-4">
-      {leftLists.map((item, i) => (
-        <div
-          key={i}
-          className="flex items-center justify-between gap-2 lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base text-white opacity-50 lg:opacity-90 rounded-lg bg-[#1B1C1E] xs:min-w-[100px] sm:min-w-[120px] md:min-w-[150px]"
-        >
-          <span className="flex-1 text-left">{item.name}</span>
-          <img src={item.icon} alt={item.name} className="w-6 h-6" />
-        </div>
-      ))}
-      <span className="lg:py-4 lg:px-3 py-3 px-2 rounded-lg bg-[#1B1C1E]"></span>
-    </div>
-    <div className="flex flex-col gap-2 md:gap-2 lg:gap-4">
-      <span className="lg:py-4 lg:px-3 py-4 px-3 rounded-lg bg-[#1B1C1E]"></span>
-      {rightLists.map((item, i) => (
-        <div
-          key={i}
-          className="flex items-center justify-between gap-2 lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base text-white opacity-50 lg:opacity-90 rounded-lg bg-[#1B1C1E] xs:min-w-[100px] sm:min-w-[120px] md:min-w-[150px]"
-        >
-          <span className="flex-1 text-left">{item.name}</span>
-          <img src={item.icon} alt={item.name} className="w-6 h-6" />
-        </div>
-      ))}
-    </div>
-  </div>
-)}
-{ id === 2 && 
-<div >
-  <img src={img} alt="img" className="w-full h-full opacity-80 sm:w-40 sm:h-40 md:relative lg:relative lg:left-[-16] md:left-[-16] sm:absolute sm:top-0 sm:right-0 sm:opacity-80" />
-  
-</div>}
+            {id === 1 && (
+              <div className=" flex gap-1 lg:gap-2 w-fit absolute -right-0 xs:mt-100 sm:mt-60 md:mt-0 lg:mt-0">
+                <div className=" flex flex-col gap-2 md:gap-2 lg:gap-4">
+                  {leftLists.map((item, i) => (
+                    <div
+                      key={i}
+                      className="flex items-center justify-between gap-2 lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base text-white opacity-50 lg:opacity-90 rounded-lg bg-[#1B1C1E] xs:min-w-[100px] sm:min-w-[120px] md:min-w-[150px]"
+                    >
+                      <span className="flex-1 text-left">{item.name}</span>
+                      <img
+                        src={item.icon}
+                        alt={item.name}
+                        className="w-6 h-6"
+                      />
+                    </div>
+                  ))}
+                  <span className="lg:py-4 lg:px-3 py-3 px-2 rounded-lg bg-[#1B1C1E]"></span>
+                </div>
+                <div className="flex flex-col gap-2 md:gap-2 lg:gap-4">
+                  <span className="lg:py-4 lg:px-3 py-4 px-3 rounded-lg bg-[#1B1C1E]"></span>
+                  {rightLists.map((item, i) => (
+                    <div
+                      key={i}
+                      className="flex items-center justify-between gap-2 lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base text-white opacity-50 lg:opacity-90 rounded-lg bg-[#1B1C1E] xs:min-w-[100px] sm:min-w-[120px] md:min-w-[150px]"
+                    >
+                      <span className="flex-1 text-left">{item.name}</span>
+                      <img
+                        src={item.icon}
+                        alt={item.name}
+                        className="w-6 h-6"
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+            {id === 2 && (
+              <div>
+                <img
+                  src={img}
+                  alt="img"
+                  className="w-full h-full opacity-80 sm:w-40 sm:h-40 md:relative lg:relative lg:left-[-16] md:left-[-16] sm:absolute sm:top-0 sm:right-0 sm:opacity-80"
+                />
+              </div>
+            )}
 
-{ id === 3 && 
-  <div>
-    <div className="left-[-8] top-[-90] w-full h-full">
-      <GridBackgroundDemo />  
-    </div>
-    <Magicbutton link="/myprojects/2" className="mb-16 pb-5 mr-9 mt-[-0px]" text="learn more" />
-  </div>
-}
-
-        </div>}
-
-
-
+            {id === 3 && (
+              <div>
+                <div className="left-[-8] top-[-90] w-full h-full">
+                  <GridBackgroundDemo />
+                </div>
+                <Magicbutton
+                  link="/myprojects/2"
+                  className="mb-16 pb-5 mr-9 mt-[-0px]"
+                  text="learn more"
+                />
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
